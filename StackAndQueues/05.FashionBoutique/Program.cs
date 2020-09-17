82fe5ac705b18f204c3a0e6c9f98154a45714657
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-09-18 00:36:26 +0100

    Add Fashion Boutique Homework

---
+++ StackAndQueues/05.FashionBoutique/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothesValue = Console.ReadLine().
+                Split().
+                Select(int.Parse).
+                ToArray();
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>(clothesValue);
+
+            int rack = 1;
+            int valueSum = 0;
+
+            while (stack.Any())
+            {
+                if (valueSum >= capacity)
+                {
+                    rack++;
+
+                    if (valueSum == capacity)
+                    {
+                        valueSum = 0;
+                        continue;
+                    }
+
+                }
+                else
+                {     
+                    if (valueSum + stack.Peek() > capacity)
+                    {
+                        rack++;
+                        valueSum = 0;
+                        continue;
+                    }
+                    else
+                    {
+                        valueSum += stack.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine(rack);
+        }
+    }
+}
