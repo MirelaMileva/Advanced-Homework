d8ea6417c3e6ab76a32c01ebdc330fa05b2b4767
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-09-17 23:01:13 +0100

    Add Fast Food Homework

---
+++ StackAndQueues/04.FastFood/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+
+            var orders = Console.ReadLine().
+                Split().
+                Select(int.Parse).
+                ToArray();
+
+            var queue = new Queue<int>(orders);
+
+            Console.WriteLine(queue.Max());
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                if (food >= orders[i] && queue.Count > 0)
+                {
+                    if (queue.Any())
+                    {
+                        queue.Dequeue();
+                        food -= orders[i];
+                    }
+                    
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            if (queue.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
