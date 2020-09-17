1f8abfa8b373f664dbcf2f46776fb6a749ce435b
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-09-18 00:11:09 +0100

    Add Songs Queue And Truck Tour Homework

---
+++ StackAndQueues/06.SongsQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ");
+
+            var songs = new Queue<string>(input);
+
+            while (songs.Count > 0)
+            {
+                string commands = Console.ReadLine();
+
+                if (commands.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (commands.Contains("Add"))
+                {
+                    string songToAdd = commands.Substring(4, commands.Length - 4);
+
+                    if (songs.Contains(songToAdd))
+                    {
+                        Console.WriteLine($"{songToAdd} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(songToAdd);
+                    }
+                    
+                }
+                else if (commands.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ StackAndQueues/07.TruckTour/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            FillQueue(n, pumps);
+
+            int count = 0;
+
+            while (true)
+            {
+                int fuel = 0;
+                bool isFound = true;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int[] currentPump = pumps.Dequeue();
+
+                    fuel += currentPump[0];
+
+                    if (fuel < currentPump[1])
+                    {
+                        isFound = false;
+                    }
+
+                    fuel -= currentPump[1];
+
+                    pumps.Enqueue(currentPump);
+                }
+
+                if (isFound)
+                {
+                    break;
+                }
+
+                count++;
+
+                pumps.Enqueue(pumps.Dequeue());
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void FillQueue(int n, Queue<int[]> pumps)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int[] currPump = Console.ReadLine().
+                    Split().
+                    Select(int.Parse).
+                    ToArray();
+
+                pumps.Enqueue(currPump);
+            }
+        }
+    }
+}
