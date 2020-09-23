927c6ef9fb178656d9d43524820cfb1744f781f1
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-09-23 22:17:14 +0100

    Add Maximal Sum Homework

---
+++ MultidimensionalArrays/3.MaximalSum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().
+               Split().
+               Select(int.Parse).
+               ToArray();
+
+            int[,] matrix = new int[dimentions[0], dimentions[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine().
+                    Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                    Select(int.Parse).
+                    ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int targetRow = 0;
+            int targetCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int currSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (currSum > maxSum)
+                    {
+                        maxSum = currSum;
+                        targetRow = row;
+                        targetCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = targetRow; row <= targetRow + 2; row++)
+            {
+                for (int col = targetCol; col <= targetCol + 2; col++)
+                { 
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
