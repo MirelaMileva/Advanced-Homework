b2cc559198bd4689ff96a5248c68ef5322cfebdd
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-09-27 23:42:04 +0100

    Add Sets Of Elements

---
+++ SetsAndDictionaries/02.SetsOfElements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HashSet<int> firstSet = new HashSet<int>();
+            //HashSet<int> secondSet = new HashSet<int>();
+
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = nums[0];
+            int m = nums[1];
+
+            List<int> firstRow = new List<int>();
+            List<int> secondRow = new List<int>();
+            HashSet<int> repeatedNumber = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstRow.Add(number);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondRow.Add(number);
+            }
+
+            foreach (var num in firstRow)
+            {
+                if (firstRow.Contains(num) && secondRow.Contains(num))
+                {
+                    repeatedNumber.Add(num);
+                }
+ 
+            }
+
+            foreach (var item in repeatedNumber)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
