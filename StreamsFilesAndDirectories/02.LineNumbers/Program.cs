f988107a5f6eb26f9435fbdb9c306dff973fb5f3
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-04 02:23:29 +0100

    Add Line Numbers Homework

---
+++ StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currLine = lines[i];
+
+                int lettersCount = CountOfLetters(currLine);
+                int symbolsCount = CountOfPunctuationSymbols(currLine);
+
+                lines[i] = $"Line {i + 1}: {currLine} ({lettersCount})({symbolsCount})";
+            }
+
+            File.WriteAllLines("../../../output.txt", lines);
+
+        }
+
+        static int CountOfLetters(string line)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char current = line[i];
+
+                if (Char.IsLetter(current))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static int CountOfPunctuationSymbols(string line)
+        {
+            char[] symbols = { '-', ',', '.', '!', '?', '\'' };
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char current = line[i];
+
+                if (symbols.Contains(current))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
