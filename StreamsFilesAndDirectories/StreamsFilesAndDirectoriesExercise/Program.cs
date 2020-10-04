8e62535dcd475a7c8274f960bd76056cd210c6b1
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-04 02:21:44 +0100

    StreamsFilesAndDirectories Homework

---
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectoriesExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace StreamsFilesAndDirectoriesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var stream = new StreamReader("./text.txt");
+            int counter = 0;
+
+            while (!stream.EndOfStream)
+            {
+                var line = stream.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                if (counter % 2 == 0)
+                {
+                    line = line.Replace('-', '@');
+                    line = line.Replace(',', '@');
+                    line = line.Replace('.', '@');
+                    line = line.Replace('!', '@');
+                    line = line.Replace('?', '@');
+
+                    line = String.Join(" ", line
+                        .Split(' ')
+                        .Reverse());
+
+                    Console.WriteLine(line);
+                    
+                }
+
+                counter++;
+
+            }
+        }
+    }
+}
