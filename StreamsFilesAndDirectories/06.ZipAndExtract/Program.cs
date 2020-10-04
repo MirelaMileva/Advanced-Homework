bbc8273165a222195fa289e9251bc4501273e322
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-04 23:18:38 +0100

    Add Zip And Extract

---
+++ StreamsFilesAndDirectories/06.ZipAndExtract/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"C:\Users\mirel\Desktop\ZipAndExtract", @"C:\Users\mirel\source\repos\StreamsFilesAndDirectoriesExercise\06.ZipAndExtract\archive.zip");
+            ZipFile.ExtractToDirectory(@"C:\Users\mirel\source\repos\StreamsFilesAndDirectoriesExercise\06.ZipAndExtract\archive.zip",
+                @"C:\Users\mirel\Desktop\SoftUni");
+        }
+    }
+}
