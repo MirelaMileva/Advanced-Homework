25899e501359acb99fcbad81e3a73833cf1fe852
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-06 00:55:41 +0100

    Add Custom Min Function Homework

---
+++ FunctionalProgramming/03.CustomMinFunction/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minFunc = (nums) =>
+            {
+                int minValue = int.MaxValue;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] < minValue)
+                    {
+                        minValue = nums[i];
+                    }
+                }
+
+                return minValue;
+
+            };
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minFunc(numbers));
+        }
+    }
+}
