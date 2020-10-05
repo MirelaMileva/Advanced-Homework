2214bbc00a6703a4fd113dc0f625d35da7183f69
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-05 22:59:46 +0100

    Add Lab Demo Problems

---
+++ FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = s => s[0] == s.ToUpper()[0];
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(checker)
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", text));
+        }
+
+    }
+}
---
+++ FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(x => x * 1.2m)
+                .ToArray();
+
+            foreach (var item in prices)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+
+    }
+}
