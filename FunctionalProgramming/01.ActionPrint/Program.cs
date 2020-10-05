70d28fcc61f054d2b958bc5ca65a008e503ed9e6
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-06 00:07:17 +0100

    Add Action Print Exercise Homework

---
+++ FunctionalProgramming/01.ActionPrint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+
+            Action<string[]> printer = names => { Console.WriteLine(string.Join("\n", input)); };
+
+            printer(input);
+        }
+
+    }
+}
