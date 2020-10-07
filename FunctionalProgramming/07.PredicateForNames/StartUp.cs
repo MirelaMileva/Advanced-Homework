adc747aac49673c72df5aa1ece5e2b0bb3dc0b6c
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-07 23:19:35 +0100

    Add Predicate For Names Homework

---
+++ FunctionalProgramming/07.PredicateForNames/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nameLenght = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split()
+                .Where(n => n.Length <= nameLenght)
+                .ToList();
+
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
