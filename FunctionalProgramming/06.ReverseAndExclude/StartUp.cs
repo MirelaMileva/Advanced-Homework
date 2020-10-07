b9c57142540c0dca5a6a9e022423f2296a9ed7f9
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-07 23:18:21 +0100

    Add Reverse And Exclude Homework

---
+++ FunctionalProgramming/06.ReverseAndExclude/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+           List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers = numbers.Where(x => x % n != 0).ToList();
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+
+    }
+}
