33a2dd0e4f90b7f65a20d03d82518eba6249c3bd
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-11 00:43:54 +0100

    Add Define A Class Person Homework

---
+++ DefiningClasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using _01.DefineAClassPerson;
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person("Pesho", 20);
+            var secondPerson = new Person("Gosho", 18);
+            var thirdPerson = new Person("Stamat", 43);
+    
+        }
+    }
+}
