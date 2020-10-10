cc7221d2401b8eec9c35fe40e3a662e1e65783c9
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-11 00:45:34 +0100

    Add Oldest Family Member Homework

---
+++ DefiningClasses/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get;set; }
+
+        public void AddMembers(Person member)
+        {
+            People.Add(member);   
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = int.MinValue;
+            Person person = null;
+
+            foreach (var currPerson in People)
+            {
+                var currentAge = currPerson.Age;
+                if (currentAge > maxAge)
+                {
+                    maxAge = currentAge;
+                    person = currPerson;
+                }
+            }
+
+            return person;
+        }
+    }
+}
