bca196d8360c63cb113feabe58435e5ac48849dd
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-11 00:47:21 +0100

    Add Date Modifier Homework

---
+++ DefiningClasses/05.DateModifier/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+            DateModifier dateModifier = new DateModifier();
+
+            var result = dateModifier.GetDaysDifference(startDate, endDate);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
