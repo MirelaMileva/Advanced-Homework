103c43e7d5084f503f1a400e2987197aac516722
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-18 15:50:16 +0100

    Add Flower Wreaths Problem

---
+++ Exams/01.FlowerWreaths/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPrepAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liliesInput = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] rosesInput = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int wreath = 0;
+            int storedLiliesAndRoses = 0;
+
+            Stack<int> lilies = new Stack<int>(liliesInput);
+            Queue<int> roses = new Queue<int>(rosesInput);
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                var currentLilie = lilies.Peek();
+                var currentRose = roses.Peek();
+
+                if (roses.Count == 0)
+                {
+                    break;
+                }
+
+                if (currentLilie + currentRose == 15)
+                {
+                    wreath++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+                else if (currentLilie + currentRose > 15)
+                {
+                    lilies.Pop();
+                    lilies.Push(currentLilie - 2);
+                }
+                else if (currentLilie + currentRose < 15)
+                {
+                    storedLiliesAndRoses += currentLilie + currentRose;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+            }
+
+            if (storedLiliesAndRoses > 15)
+            {
+                while (storedLiliesAndRoses > 15)
+                {
+                    storedLiliesAndRoses -= 15;
+                    wreath++;
+                }
+            }
+
+            if (wreath >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreath} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreath} wreaths more!");
+            }
+        }
+    }
+}
