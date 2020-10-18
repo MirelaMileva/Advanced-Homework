c3973a2203b8718854dac5f3e340e4779a965d12
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-19 00:13:43 +0100

    Add Generic Array Creator

---
+++ Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] array = ArrayCreator.Create(5, "Pesho");
+            Console.WriteLine(string.Join(",", array));
+
+            int[] secondArray = ArrayCreator.Create(10, 33);
+            Console.WriteLine(string.Join(",", secondArray));
+        }
+    }
+}
