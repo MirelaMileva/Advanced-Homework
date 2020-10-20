fa126b116c43bcdff3e983e7d562f2c570aa5abb
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-20 23:54:55 +0100

    Add Generic Swap Method Integer

---
+++ Generics/04.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using GenericBoxOfString;
+using System;
+using System.Linq;
+
+namespace GenericsExercise
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                box.Values.Add(number);
+            }
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int a = indexes[0];
+            int b = indexes[1];
+
+            box.Swap(a, b);
+
+            Console.WriteLine(box);
+        }
+    }
+}
