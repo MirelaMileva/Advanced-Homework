4947d5fc2cac2b86b2411cc2249850809d605cf1
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-21 00:21:57 +0100

    Add Generic Count Method String

---
+++ Generics/05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.GenericCountMethodString
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Values.Add(input);
+            }
+
+            string targetItem = Console.ReadLine();
+
+           int result = box.GreaterValueThan(targetItem);
+
+            Console.WriteLine(result);
+        }
+    }
+}
