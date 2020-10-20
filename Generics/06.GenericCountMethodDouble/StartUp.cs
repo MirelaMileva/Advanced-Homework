a2c4dd9a08321a158826d349f0a802115bb4ecc5
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-21 00:28:09 +0100

    Add Generic Count Method Double

---
+++ Generics/06.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+
+                box.Values.Add(number);
+            }
+
+            double targetItem = double.Parse(Console.ReadLine());
+
+            double result = box.GreaterValueThan(targetItem);
+
+            Console.WriteLine(result);
+        }
+    }
+}
