6ef8f71dab824c5b7ff5932ee7d1109714fba2a1
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-26 23:27:54 +0000

    Add Recursive Factorial

---
+++ BasicAlgorithmsAdvanced/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            var result = FactorialSum(number);
+
+            Console.WriteLine(result);
+        }
+
+        private static long FactorialSum(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * FactorialSum(number - 1);
+        }
+    }
+}
